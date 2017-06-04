674a56ccd249d5df13b9329cece162683b2627c8
Guillermo Greco Jr <ggreco@guillermos-mbp.attlocal.net>
2017-06-04 15:13:53 -0700

    Made the facebook button round and the top layer where the logo is located, i added shadow.

---
+++ devslopes-social/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  devslopes-social
+//
+//  Created by Guillermo Greco Jr. on 6/4/17.
+//  Copyright © 2017 Guillermo Greco Jr. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 120.0/255.0
---
+++ devslopes-social/FancyView.swift
@@ -0,0 +1,22 @@
+//
+//  FancyView.swift
+//  devslopes-social
+//
+//  Created by Guillermo Greco Jr. on 6/4/17.
+//  Copyright © 2017 Guillermo Greco Jr. All rights reserved.
+//
+
+import UIKit
+
+class FancyView: UIView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_COLOR, green: SHADOW_COLOR, blue: SHADOW_COLOR, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 5.0, height: 1.0)
+    }
+
+}
